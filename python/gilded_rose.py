eba887ddf859b19eda655f533b1885e5e14e30a8
zty <zty20010131@gmail.com>
2026-03-16 19:25:22 -0700

    v1

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -10,6 +10,84 @@ class Item:
         return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
 
 
+class ItemUpdater:
+    """Strategy base class: one updater per item type."""
+
+    def __init__(self, item: Item):
+        self.item = item
+
+    def update(self):
+        raise NotImplementedError
+
+    def _decrease_sell_in(self):
+        self.item.sell_in -= 1
+
+    def _increase_quality(self, amount: int = 1):
+        self.item.quality = min(50, self.item.quality + amount)
+
+    def _decrease_quality(self, amount: int = 1):
+        self.item.quality = max(0, self.item.quality - amount)
+
+
+class NormalItemUpdater(ItemUpdater):
+    def update(self):
+        self._decrease_quality()
+        self._decrease_sell_in()
+        if self.item.sell_in < 0:
+            self._decrease_quality()
+
+
+class AgedBrieUpdater(ItemUpdater):
+    def update(self):
+        self._increase_quality()
+        self._decrease_sell_in()
+        if self.item.sell_in < 0:
+            self._increase_quality()
+
+
+class SulfurasUpdater(ItemUpdater):
+    def update(self):
+        # Legendary item: no changes to sell_in or quality
+        pass
+
+
+class BackstagePassUpdater(ItemUpdater):
+    def update(self):
+        if self.item.sell_in > 10:
+            self._increase_quality()
+        elif self.item.sell_in > 5:
+            self._increase_quality(2)
+        elif self.item.sell_in >= 0:
+            self._increase_quality(3)
+
+        self._decrease_sell_in()
+
+        # After the concert (sell_in < 0) quality drops to 0
+        if self.item.sell_in < 0:
+            self.item.quality = 0
+
+
+class ConjuredItemUpdater(ItemUpdater):
+    def update(self):
+        self._decrease_quality(2)
+        self._decrease_sell_in()
+        if self.item.sell_in < 0:
+            self._decrease_quality(2)
+
+
+def get_updater_for(item: Item) -> ItemUpdater:
+    """Factory method returning the correct strategy for an item."""
+    if item.name == "Aged Brie":
+        return AgedBrieUpdater(item)
+    if item.name == "Sulfuras, Hand of Ragnaros":
+        return SulfurasUpdater(item)
+    if item.name.startswith("Backstage passes"):
+        return BackstagePassUpdater(item)
+    if item.name.startswith("Conjured"):
+        return ConjuredItemUpdater(item)
+    return NormalItemUpdater(item)
+
+
 class GildedRose(object):
 
     def __init__(self, items):
@@ -17,31 +95,6 @@ class GildedRose(object):
 
     def update_quality(self):
         for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
-            else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
-                else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
+            updater = get_updater_for(item)
+            updater.update()
 
--- python/tests/test_gilded_rose.py
+++ python/tests/test_gilded_rose.py
@@ -5,12 +5,39 @@ from gilded_rose import Item, GildedRose
 
 
 class GildedRoseTest(unittest.TestCase):
-    def test_foo(self):
-        items = [Item("foo", 0, 0)]
-        gilded_rose = GildedRose(items)
-        gilded_rose.update_quality()
-        self.assertEqual("fixme", items[0].name)
+    def _update(self, item, days=1):
+        """Helper: run update_quality for given days on a single item."""
+        gr = GildedRose([item])
+        for _ in range(days):
+            gr.update_quality()
+        return item
 
-        
+    def test_normal_item_degrades_by_one_before_sell_date(self):
+        items = [Item("+5 Dexterity Vest", 10, 20)]
+        gr = GildedRose(items)
+        gr.update_quality()
+        self.assertEqual(9, items[0].sell_in)
+        self.assertEqual(19, items[0].quality)
+
+    def test_aged_brie_increases_in_quality_up_to_50(self):
+        item = Item("Aged Brie", 2, 49)
+        self._update(item)
+        self.assertEqual(1, item.sell_in)
+        self.assertEqual(50, item.quality)
+
+    def test_sulfuras_never_changes(self):
+        item = Item("Sulfuras, Hand of Ragnaros", 0, 80)
+        self._update(item)
+        self.assertEqual(0, item.sell_in)
+        self.assertEqual(80, item.quality)
+
+    def test_backstage_passes_drop_to_zero_after_concert(self):
+        item = Item("Backstage passes to a TAFKAL80ETC concert", 1, 49)
+        # two days so that sell_in goes from 1 -> 0 -> -1
+        self._update(item, days=2)
+        self.assertEqual(-1, item.sell_in)
+        self.assertEqual(0, item.quality)
+
+
 if __name__ == '__main__':
     unittest.main()
